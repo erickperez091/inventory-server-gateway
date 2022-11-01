08a1b201ca169108583e78ffe027477a1eff693c
Erick <erickperez091@hotmail.com>
2022-11-01 15:30:51 -0600

    Migrating to Java 17
    Migrating to Java 17
    Add routing and rewriting routes for service discovery

--- src/main/java/com/example/gateway/routing/RoutingMapConf.java
+++ src/main/java/com/example/gateway/routing/RoutingMapConf.java
@@ -13,7 +13,18 @@ public class RoutingMapConf {
     @Bean
     public RouteLocator configure( RouteLocatorBuilder routeLocatorBuilder ){
         return routeLocatorBuilder.routes()
-                .route( "inventory-service", route -> route.path( "/**" ).uri( "lb://inventory-service" ))
+                .route( "inventory-service", route -> route
+                        .path( "/api/product/v1/**" ).or().path( "/api/category/v1/**" )
+                        .filters( filter -> filter
+                                .rewritePath( "/api/(?<segment>/?.*)", "/$\\{segment}" ))
+                        .uri( "lb://inventory-service" ))
+
+                .route( "invoice-service", route -> route
+                        .path( "/api/invoice/v1/**" )
+                        .filters( filter -> filter
+                                .rewritePath( "/api/(?<segment>/?.*)", "/$\\{segment}" ))
+                        .uri( "lb://invoice-service" ))
+
                 .build();
     }
 
